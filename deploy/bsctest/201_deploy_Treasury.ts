6716aff153e9c18a3644ad667a820c553af236a3
kritical0613 <kulishan0613@gmail.com>
2022-05-11 02:01:28 -0500

    fix(gemo): treasury deploy script

--- deploy/bsctest/201_deploy_Treasury.ts
+++ deploy/bsctest/201_deploy_Treasury.ts
@@ -1,8 +1,8 @@
-import { run } from "hardhat";
+import { run, ethers } from "hardhat";
 import { DeployFunction } from "hardhat-deploy/types";
 import getDeploymentAddresses from "../../utils/readStatic"
 
-const func: DeployFunction = async({getNamedAccounts, deployments, network}) => {
+const func: DeployFunction = async ({ getNamedAccounts, deployments, network }) => {
   console.log("> (201) Deploy Treasury:");
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
@@ -14,19 +14,23 @@ const func: DeployFunction = async({getNamedAccounts, deployments, network}) => 
   const result = await deploy("Treasury", {
     log: true,
     from: deployer,
-    args: [emoAddr, gemoAddr] 
+    args: [emoAddr, gemoAddr]
   });
 
-  // Verify contract
-  if(result.newlyDeployed) {
+  // Verify & Initial Setting contract
+  if (result.newlyDeployed) {
     if (network.live) {
       await run("verify:verify", {
         address: result.address,
         constructorArguments: [emoAddr, gemoAddr],
         contract: "contracts/gemo/Treasury.sol:Treasury"
       });
+
+      const gEmoToken = await ethers.getContract("GemEMO");
+      await gEmoToken.excludeAccount(result.address)
     }
   }
+
 }
 
 export default func;
